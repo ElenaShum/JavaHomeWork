1b529e5464b90c8fe9bf3be048b60cf39a66a198
ElenaShum <97665478+ElenaShum@users.noreply.github.com>
2023-08-23 14:55:29 +0300

    Add files via upload
    Написать программу игру “Bingo”. Используя IntelliJ IDEA, создайте класс Bingo.
    Программа должна загадать целое число в диапазоне от 0 до 100 и предложить пользователю его отгадать.
    При вводе числа пользователем, программа проверяет на соответствие с загаданным число и если числа совпали вывести сообщение о том что число отгадано. Если числа не совпали, тогда следует вывести надпись о том, что задуманное число является больше или меньше вводимого. Также программа ведет подсчет попыток, и выводит это количество после того как число угадали. Предусмотреть возможность досрочного завершения программы, в случае если пользователь не желает продолжать угадывать число.

---
+++ Bingo.java
@@ -0,0 +1,69 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Bingo {
+    public static void main(String[] args) {
+        System.out.println("Угадайте число от 1 до 100 или введите out для выхода из программы");
+
+        int attempt = 0; // кол-во попыток
+        // загадываем число
+        Random rand = new Random();
+        int x = 0;
+        x = rand.nextInt(100) + 1 ;
+        Scanner sc = new Scanner(System.in);
+
+        int number = 0;
+
+
+        while (true) {
+            attempt++;
+            System.out.println("Попытка " + attempt + ":");
+            String in = sc.next();
+
+
+            if (in.equals("out")) {
+                System.out.println("Программа окончена");
+                break;
+            }
+
+            try {
+                 number = Integer.parseInt(in);
+            } catch (Exception e) {
+                System.out.println("Вы ввели не число (" + in + ")");
+            }
+
+
+
+            if (number > x) {
+                System.out.println("Искомое число меньше");
+                continue;
+            }
+
+            if (number < x) {
+                System.out.println("Искомое число больше");
+            }
+
+            if (number != x) {
+                continue;
+            }
+
+            System.out.println("Вы угадали с " + attempt + " попытки");
+            break;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+//        for (int i = 0; i < 10; i++) {
+//            int x = rand.nextInt(10);
+//            System.out.println(x);
+
+
+
